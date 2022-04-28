dd93249cc2298ba666e380c36eeb4ec816889aae
asmae din <assouma4ever@gmail.com>
2022-04-28 15:28:01 +0000

    up

--- function.php
+++ function.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     include_once("config.php");
+    
+
     /* Page authentification */
     if (isset($_POST['email']) and isset($_POST['password'])){
         $query = $cnx->prepare("SELECT * FROM etudiant WHERE email = ? and password = ?");
@@ -25,25 +27,22 @@
     // set the PDO error mode to exception
     $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       // prepare sql and bind parameters
-    $stmt = $cnx->prepare("INSERT INTO etudiant (nom_prenom, date_naiss, email, tel)
-    VALUES (:nom_prenom, :date_naiss, :email, :tel)");
-    $stmt->bindParam(':nom_prenom', $nom_prenom);
-    $stmt->bindParam(':date_naiss', $date_naiss);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':tel', $tel);
-
-
     $nom_prenom= $_POST["nom_prenom"];
     $date_naiss = $_POST["date_naiss"];
+    $genre=$_POST["gender"];
     $email = $_POST["email"];
     $tel = $_POST["tel"];
-    $stmt->execute();
     $_SESSION["nom_complet"] = $nom_prenom;
+    $stmt = $cnx->prepare("INSERT INTO etudiant (nom_prenom, date_naiss, genre, email, tel)
+    VALUES ('$nom_prenom','$date_naiss','$genre','$email','$tel')");
+    $stmt->execute();
+
+    
     echo "New records created successfully";
     header("location:./profile.php?msg=$msg");
   } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
   }
-  $conn = null;
+  $cnx = null;
 
 ?>
--- register.php
+++ register.php
@@ -8,60 +8,68 @@
             <link rel="stylesheet" href="./styles-regis.css"/>
         </head>
         <body>
-                <form class="registration-container" method="POST" action="function.php" onsubmit="checkPw()">
+                <form class="registration-container" method="POST" action="function.php"  onsubmit="checkGender()">
                     <h1 class="header">Registration</h1>
-                            <div class="header-underline"></div>
-                            <div class="fields-container">
-                                <div class="inputs-container">
-                                    <label class="field-label" for="name">Nom et Prénom</label>
-                                    <input  class="input-field" type="text" placeholder="Nom & Prenom" name="nom_prenom" >
-                                </div>
-                                <div class="inputs-container">
-                                    <label class="field-label" for="username">Date de naissance</label> 
-                                    <input class="input-field" type="text" placeholder="Date de naissance" name="date_naiss" > 
-                                </div>
-                                <div class="inputs-container">
-                                    <label class="field-label" for="email">Email</label> 
-                                    <input class="input-field" type="email" placeholder="Email" name="email" > 
-                                </div>
-                                <div class="inputs-container">
-                                    <label class="field-label" for="phone-number">Tel:</label> 
-                                    <input  class="input-field" type="tel" placeholder="Phone number" name="tel" > 
-                                </div>
-                                <div class="inputs-container">
-                                    <label class="field-label" for="password">Password</label> 
-                                    <input class="input-field" type="password" placeholder="Password" name="pass" id="pass"> 
-                                </div>
-                                <div class="inputs-container">
-                                    <label class="field-label" for="confi-password">Confirm Password</label>
-                                    <input  class="input-field" type="password" placeholder="Confirm your Password" name="confirm-password" id="confirm-pass"> 
-                                </div>
+                    <div class="header-underline"></div>
+                    <div class="fields-container">
+                        <div class="inputs-container">
+                            <label class="field-label" for="name">Nom et Prénom</label>
+                            <input  class="input-field" type="text" placeholder="Nom & Prenom" name="nom_prenom" >
+                        </div>
+                        <div class="inputs-container">
+                            <label class="field-label" for="username">Date de naissance</label> 
+                            <input class="input-field" type="text" placeholder="Date de naissance" name="date_naiss" > 
+                        </div>
+                        <div class="inputs-container">
+                            <label class="field-label" for="email">Email</label> 
+                            <input class="input-field" type="email" placeholder="Email" name="email" > 
+                        </div>
+                        <div class="inputs-container">
+                            <label class="field-label" for="phone-number">Tel:</label> 
+                            <input  class="input-field" type="tel" placeholder="Phone number" name="tel" > 
+                        </div>
+                        <div class="inputs-container">
+                            <label class="field-label" for="password">Password</label> 
+                            <input class="input-field" type="password" placeholder="Password" name="pass" id="pass"> 
+                        </div>
+                        <div class="inputs-container">
+                            <label class="field-label" for="confi-password">Confirm Password</label>
+                            <input  class="input-field" type="password" placeholder="Confirm your Password" name="confirm-password" id="confirm-pass"> 
+                        </div>
                             
-                                <div  class="inputs-container gender-fields">
-                                    <label class="field-label">Gender</label>
-                                        <div class="gender-inputs">
-                                            <input type="radio">
-                                            <label  class="gender-lbl">Male</label>
-                                            <input type="radio">
-                                            <label  class="gender-lbl">female</label>
-                                            <input type="radio">
-                                            <label  class="gender-lbl">Prefer not to say</label>
-                                        </div>
-                                </div> 
-                                <button class="btn ">Register</button>
+                        <div  class="inputs-container gender-fields">
+                            <label class="field-label">Gender</label>
+                            <div class="gender-inputs">
+                                <input type="radio" name="gender" value="male" >
+                                <label  class="gender-lbl" for="male">Male</label>
+                                <input type="radio" name="gender" value="female">
+                                <label  class="gender-lbl" for="female" >Female</label>
+                                <input type="radio" name="gender" value="Prefer not to say">
+                                <label  class="gender-lbl" for="Prefer not to say">Prefer not to say</label>
+                                <label for="gender_error" id="gender_error"></label>
                             </div>
-                </form>
-                <script>
-                                function checkPw() {
-                                    alert("hi");
-                                pw1 = document.getElementById("pass").value;
-                                pw2 = document.getElementById("confirm-pass").value;
-                                if (pw1 != pw2) {
-                                alert("\erreur: les mots de passes ne correspondent pas")
-                                return false;
-                                }
-                                else return true;
-                                }
-                            </script>
+                         </div> 
+                        <button class="btn " name="submit">Register</button>
+                    </div>
+             </form>
+                    <script>
+
+                        function checkPw() 
+                        {
+                            pw1 = document.getElementById("pass").value;
+                            pw2 = document.getElementById("confirm-pass").value;
+                            if (pw1 != pw2) {
+                            alert("\erreur: les mots de passes ne correspondent pas")
+                            return false;
+                            }else{
+                            return true;
+                            }
+                        }
+                        const sex=document.querySelector('input[name="gender"]');
+                        const btn=document.getElementsByName('submit');
+                        const genderLbl=document.getElementById('gender_error');
+                        
+                    
+                    </script>
         </body>
 </html>
