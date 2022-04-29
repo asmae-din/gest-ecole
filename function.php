af273b6828eb70d2f1475cca1f86521ac79bd6a3
asmae din <assouma4ever@gmail.com>
2022-04-29 14:02:36 +0000

    up

--- function.php
+++ function.php
@@ -22,27 +22,33 @@
             header("location:./signin.php?msg=$msg");
         }
     }
+    if($user){
+      try {
 
-  try {
-    // set the PDO error mode to exception
-    $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      // prepare sql and bind parameters
-    $nom_prenom= $_POST["nom_prenom"];
-    $date_naiss = $_POST["date_naiss"];
-    $genre=$_POST["gender"];
-    $email = $_POST["email"];
-    $tel = $_POST["tel"];
-    $_SESSION["nom_complet"] = $nom_prenom;
-    $stmt = $cnx->prepare("INSERT INTO etudiant (nom_prenom, date_naiss, genre, email, tel)
-    VALUES ('$nom_prenom','$date_naiss','$genre','$email','$tel')");
-    $stmt->execute();
+        // set the PDO error mode to exception
+        $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          // prepare sql and bind parameters
+        $nom_prenom= $_POST["nom_prenom"];
+        $date_naiss = $_POST["date_naiss"];
+        $genre=$_POST["gender"];
+        $email = $_POST["email"];
+        $tel = $_POST["tel"];
+        $pw=$_POST["pass"];
+        $_SESSION["nom_complet"] = $nom_prenom;
+        $stmt = $cnx->prepare("INSERT INTO etudiant (nom_prenom, date_naiss, genre, email, tel, password)
+        VALUES ('$nom_prenom','$date_naiss','$genre','$email','$tel','$pw')");
+        $stmt->execute();
 
-    
-    echo "New records created successfully";
-    header("location:./profile.php?msg=$msg");
-  } catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-  }
-  $cnx = null;
+        
+        echo "New records created successfully";
+        header("location:./profile.php?msg=$msg");
+      } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+      }
+      $cnx = null;
+    }else{
+      
+
+}
 
 ?>
--- register.php
+++ register.php
@@ -8,41 +8,41 @@
             <link rel="stylesheet" href="./styles-regis.css"/>
         </head>
         <body>
-                <form class="registration-container" method="POST" action="function.php"  onsubmit="checkGender()">
+                <form class="registration-container" method="POST" action="function.php"  onsubmit="checkPw() & checkGender()">
                     <h1 class="header">Registration</h1>
                     <div class="header-underline"></div>
                     <div class="fields-container">
                         <div class="inputs-container">
                             <label class="field-label" for="name">Nom et Prénom</label>
-                            <input  class="input-field" type="text" placeholder="Nom & Prenom" name="nom_prenom" >
+                            <input  class="input-field" type="text" placeholder="Nom & Prenom" name="nom_prenom" required>
                         </div>
                         <div class="inputs-container">
                             <label class="field-label" for="username">Date de naissance</label> 
-                            <input class="input-field" type="text" placeholder="Date de naissance" name="date_naiss" > 
+                            <input class="input-field" type="text" placeholder="Date de naissance" name="date_naiss" required > 
                         </div>
                         <div class="inputs-container">
                             <label class="field-label" for="email">Email</label> 
-                            <input class="input-field" type="email" placeholder="Email" name="email" > 
+                            <input class="input-field" type="email" placeholder="Email" name="email"  required> 
                         </div>
                         <div class="inputs-container">
                             <label class="field-label" for="phone-number">Tel:</label> 
-                            <input  class="input-field" type="tel" placeholder="Phone number" name="tel" > 
+                            <input  class="input-field" type="tel" placeholder="Phone number" name="tel" required > 
                         </div>
                         <div class="inputs-container">
                             <label class="field-label" for="password">Password</label> 
-                            <input class="input-field" type="password" placeholder="Password" name="pass" id="pass"> 
+                            <input class="input-field" type="password" placeholder="Password" name="pass" id="pass" required > 
                         </div>
                         <div class="inputs-container">
                             <label class="field-label" for="confi-password">Confirm Password</label>
-                            <input  class="input-field" type="password" placeholder="Confirm your Password" name="confirm-password" id="confirm-pass"> 
+                            <input  class="input-field" type="password" placeholder="Confirm your Password" name="confirm-password" id="confirm-pass" required> 
                         </div>
                             
                         <div  class="inputs-container gender-fields">
                             <label class="field-label">Gender</label>
-                            <div class="gender-inputs">
+                            <div class="gender-inputs" aria-required="$_POST">
                                 <input type="radio" name="gender" value="male" >
                                 <label  class="gender-lbl" for="male">Male</label>
-                                <input type="radio" name="gender" value="female">
+                                <input type="radio" name="gender" value="female" >
                                 <label  class="gender-lbl" for="female" >Female</label>
                                 <input type="radio" name="gender" value="Prefer not to say">
                                 <label  class="gender-lbl" for="Prefer not to say">Prefer not to say</label>
@@ -59,16 +59,24 @@
                             pw1 = document.getElementById("pass").value;
                             pw2 = document.getElementById("confirm-pass").value;
                             if (pw1 != pw2) {
-                            alert("\erreur: les mots de passes ne correspondent pas")
+                            alert("\erreur: les mots de passes ne correspondent pas");
                             return false;
                             }else{
                             return true;
                             }
                         }
-                        const sex=document.querySelector('input[name="gender"]');
-                        const btn=document.getElementsByName('submit');
-                        const genderLbl=document.getElementById('gender_error');
                         
+                       
+                        function checkGender()
+                        {
+                           
+                            var checked_gender = document.querySelector('input[name = "gender"]:checked');
+                            if(checked_gender != null){  
+                            return true; 
+                            } else {
+                            alert('Nothing checked');
+                            }
+                        }
                     
                     </script>
         </body>
